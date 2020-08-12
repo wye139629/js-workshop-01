b6c9b543acd2e20316345312758d6d822d03d165
will Liu <wye139629@gmail.com>
2020-08-12 18:01:12 +0800

    finish drag

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,12 +1,10 @@
 window.addEventListener('DOMContentLoaded',function(){ 
   let ul = document.querySelector('ul')
-  const lists = document.querySelectorAll('ul>li')
   const addBtn = document.getElementById('addBtn')
 
 
   ul.addEventListener('click',function(e) {
-    let listItem = document.querySelectorAll('ul li')
-    const closes = document.querySelectorAll('.close')
+    const lists = document.querySelectorAll('ul>li')  
     console.log(e)
     if(e.target.className.indexOf('checked')!=-1){
        e.target.classList.remove('checked')
@@ -16,36 +14,8 @@ window.addEventListener('DOMContentLoaded',function(){
       if(e.target.className.indexOf("close")!=-1){
         e.target.parentNode.remove()
       }
-  })
 
-  // function checkHandler(e){
-  //   if(e.target.classList.value === 'checked'){
-  //     e.target.classList.remove('checked')
-  //   }else{
-  //     e.target.classList.add('checked')
-  //   }
-  // }  
-  // function removeHandler(e){
-  //   e.target.parentNode.remove()
-  // }
-
-  function addHandler(){
-    let listArea = document.querySelector('ul')
-    let list = document.createElement('li')
-    let span = document.createElement('span')
-    let input = document.getElementById('input')
-    list.textContent = input.value
-    span.className = 'close'
-    span.textContent = 'x'
-    list.appendChild(span)
-    listArea.appendChild(list)  
-  }
-  // function drag(e) {
-  //   let position = e.pageY-e.offsetY
-  //   console.log(position)
-
-  // }
-  
+      
   function start(e) {
     // console.log(start)
     e.target.className = 'tar'
@@ -70,11 +40,11 @@ window.addEventListener('DOMContentLoaded',function(){
     
     if(newIndex < oldIndex){
       target.insertAdjacentElement('beforebegin',oldtarget)
+      oldtarget.classList.remove('tar')
     }else{
       target.insertAdjacentElement('afterend',oldtarget)
+      oldtarget.classList.remove('tar')
     }
-    
-
   }
   // function enter(e) {
   //   // console.log(enter)
@@ -91,19 +61,50 @@ window.addEventListener('DOMContentLoaded',function(){
     e.stopPropagation()
     return false
   }
+
+
+
+    lists.forEach((list)=>{
+      list.setAttribute('draggable', true)
+      list.addEventListener('dragstart',start)
+      // list.addEventListener('drag',drag)
+      list.addEventListener('dragenter',cancelDefault)
+      list.addEventListener('dragover',cancelDefault)
+      list.addEventListener('drop',drop)
+    })
+
+  })
+
+  // function checkHandler(e){
+  //   if(e.target.classList.value === 'checked'){
+  //     e.target.classList.remove('checked')
+  //   }else{
+  //     e.target.classList.add('checked')
+  //   }
+  // }  
+  // function removeHandler(e){
+  //   e.target.parentNode.remove()
+  // }
+
+  function addHandler(){
+    let listArea = document.querySelector('ul')
+    let list = document.createElement('li')
+    let span = document.createElement('span')
+    let input = document.getElementById('input')
+    list.textContent = input.value
+    span.className = 'close'
+    span.textContent = 'x'
+    list.appendChild(span)
+    listArea.appendChild(list)  
+  }
+  
+  
   
   
   // console.log(listItem)
   // listItem.forEach((item)=>{item.addEventListener('click',checkHandler)})
   // closes.forEach((close)=>{close.addEventListener('click',removeHandler)})
   addBtn.addEventListener('click',addHandler)
-  lists.forEach((list)=>{
-    list.setAttribute('draggable', true)
-    list.addEventListener('dragstart',start)
-    // list.addEventListener('drag',drag)
-    list.addEventListener('dragenter',cancelDefault)
-    list.addEventListener('dragover',cancelDefault)
-    list.addEventListener('drop',drop)
-  })
+  
 
 })
