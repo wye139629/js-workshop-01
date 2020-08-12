0cafed470f81fb31248f6e1259345f917ce45b3e
will Liu <wye139629@gmail.com>
2020-08-12 17:05:53 +0800

    WIP on drag

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,1 +1,109 @@
-// TO DO
+window.addEventListener('DOMContentLoaded',function(){ 
+  let ul = document.querySelector('ul')
+  const lists = document.querySelectorAll('ul>li')
+  const addBtn = document.getElementById('addBtn')
+
+
+  ul.addEventListener('click',function(e) {
+    let listItem = document.querySelectorAll('ul li')
+    const closes = document.querySelectorAll('.close')
+    console.log(e)
+    if(e.target.className.indexOf('checked')!=-1){
+       e.target.classList.remove('checked')
+        }else{
+        e.target.classList.add('checked')
+      }
+      if(e.target.className.indexOf("close")!=-1){
+        e.target.parentNode.remove()
+      }
+  })
+
+  // function checkHandler(e){
+  //   if(e.target.classList.value === 'checked'){
+  //     e.target.classList.remove('checked')
+  //   }else{
+  //     e.target.classList.add('checked')
+  //   }
+  // }  
+  // function removeHandler(e){
+  //   e.target.parentNode.remove()
+  // }
+
+  function addHandler(){
+    let listArea = document.querySelector('ul')
+    let list = document.createElement('li')
+    let span = document.createElement('span')
+    let input = document.getElementById('input')
+    list.textContent = input.value
+    span.className = 'close'
+    span.textContent = 'x'
+    list.appendChild(span)
+    listArea.appendChild(list)  
+  }
+  // function drag(e) {
+  //   let position = e.pageY-e.offsetY
+  //   console.log(position)
+
+  // }
+  
+  function start(e) {
+    // console.log(start)
+    e.target.className = 'tar'
+    // console.log(e.target)
+
+    let position = e.pageY-e.offsetY
+    e.dataTransfer.setData('listP', position)
+    e.dataTransfer.setData('list',e.target.className)
+  }
+
+
+  function drop(e) {
+    cancelDefault(e)
+    let newPosition = e.pageY-e.offsetY
+    let oldIndex = e.dataTransfer.getData('listP')
+    let target = e.target
+    let newIndex = newPosition
+    let oldClassName = e.dataTransfer.getData('list')
+    let oldtarget = document.querySelector('.'+oldClassName)
+    console.log(oldtarget)
+    
+    
+    if(newIndex < oldIndex){
+      target.insertAdjacentElement('beforebegin',oldtarget)
+    }else{
+      target.insertAdjacentElement('afterend',oldtarget)
+    }
+    
+
+  }
+  // function enter(e) {
+  //   // console.log(enter)
+  //   // console.log(e)
+  //   e.preventDefault()
+  //   e.stopPropagation()
+  // }
+  
+  function over(e) {
+    // console.log(over)
+  }
+  function cancelDefault(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    return false
+  }
+  
+  
+  // console.log(listItem)
+  // listItem.forEach((item)=>{item.addEventListener('click',checkHandler)})
+  // closes.forEach((close)=>{close.addEventListener('click',removeHandler)})
+  addBtn.addEventListener('click',addHandler)
+  lists.forEach((list)=>{
+    list.setAttribute('draggable', true)
+    list.addEventListener('dragstart',start)
+    // list.addEventListener('drag',drag)
+    list.addEventListener('dragenter',cancelDefault)
+    list.addEventListener('dragover',cancelDefault)
+    list.addEventListener('drop',drop)
+  })
+
+})
--- 04_bonus-drag/main.js
+++ 04_bonus-drag/main.js
@@ -3,32 +3,42 @@ document.addEventListener("DOMContentLoaded", function(){
   let target = document.querySelector('#target')
 
   //source
-  circle.addEventListener('drag', function(){
-    console.log('drag')
-  })
+  // circle.addEventListener('drag', function(){
+  //   console.log('drag')
+  // })
 
-  circle.addEventListener('dragstart', function(){
+  circle.addEventListener('dragstart', function(e){
     console.log('dragstart')
+    e.dataTransfer.setData('circle',e.target.id)
+    // console.log(e.target.id)
   })
 
-  circle.addEventListener('dragend', function(){
-    console.log('dragend')
-  })
+  // circle.addEventListener('dragend', function(){
+  //   console.log('dragend')
+  // })
 
   // //target
-  // target.addEventListener('dragenter', function(){
-  //   console.log('dragenter target')
-  // })
+  target.addEventListener('dragenter', function(e){
+    console.log('dragenter target')
+    e.preventDefault()
+    e.stopPropagation()
+    return false
+  })
 
-  // target.addEventListener('dragover', function(){
-  //   console.log('dragover target')
-  // })
+  target.addEventListener('dragover', function(e){
+    console.log('dragover target')
+    e.preventDefault()
+    e.stopPropagation()
+    return false
+  })
 
   // target.addEventListener('dragleave', function(){
   //   console.log('dragleave target')
   // })
 
-  // target.addEventListener('drop', function(){
-  //   console.log('drop')
-  // })
+  target.addEventListener('drop', function(e){
+    console.log('drop')
+    let id = e.dataTransfer.getData('circle')
+    e.target.appendChild(document.querySelector('#'+id))
+  })
 })
